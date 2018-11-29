f3efdf8924233f186d1ca42e2674a08b9d6a43d8
Jonathan Kjølstad <jonathan.kjolstad@gmail.com>
2018-11-29 22:24:34 +0100

    Endret litt pa script.js

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-/*
-
-function closeMenu() {
-    var x = document.getElementById("dropdown");
-    x.style.display = "none";
-}
-
-function openMenu() {
-    var x = document.getElementById("dropdown");
-    x.style.display = "block";
-
-}
-
-*/
 
 function toggleMenu() {
   var x = document.getElementById("dropdown");
