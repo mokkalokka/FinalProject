45adf60b6a40897f3c24db09cb64008413adde8d
Jonathan Kjølstad <jonathan.kjolstad@gmail.com>
2018-11-29 22:19:17 +0100

    Menyknapp blir borte naar meny aapnes

--- script.js
+++ script.js
@@ -15,9 +15,12 @@ function openMenu() {
 
 function toggleMenu() {
   var x = document.getElementById("dropdown");
+  var menyKnapp = document.getElementsByClassName("roundBorder")[0];
   if (x.style.display === "block") {
     x.style.display = "none";
+    menyKnapp.style.display = "block";
   } else {
     x.style.display = "block"
+    menyKnapp.style.display = "none";
   }
 }
